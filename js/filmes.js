20d6181f185258243ced133d58ec1925aae44f4d
PIETRA PAULA VOLPATO <23111825@CFP127ALUNO.LOCAL>
2024-04-25 17:18:50 -0300

    em processo

---
+++ js/filmes.js
@@ -0,0 +1,55 @@
+//responsavel por interagir com os filmes
+
+export async function getFilmes (){
+    const url = 'http://localhost:8080/v2/acmeFilmes/filmes'
+    const response = await fetch(url)
+    const data= await response.json()
+
+   return data.filmes
+}
+
+export async function getFilme (id) {
+    const url = `http://localhost:8080/v2/acmefilmes/filme/${id}`
+    const response = await fetch(url)
+    const data= await response.json()
+
+    return data.filme[0]
+}
+
+export async function postfilme (filme) {
+   const url = 'http://localhost:8080/v2/acmefilmes/filme'
+   const options = {
+      method: 'POST',
+      headers: {
+         'Content-Type': 'application/json'
+      },
+      body: JSON.stringify (filme)
+   }
+   const response = await fetch (url, options)
+
+   return response.ok
+}
+
+export async function putfilme (filme) {
+   const url = `http://localhost:8080/v2/acmefilmes/filme/${filme.id}`
+   const options = {
+      method: 'PUT',
+      headers: {
+         'Content-Type': 'application/json'
+      },
+      body: JSON.stringify (filme)
+   }
+   const response = await fetch (url, options)
+
+   return response.ok
+}
+
+export async function deletefilme (id) {
+   const url = `http://localhost:8080/v2/acmefilmes/filme/${id}`
+   const options = {
+      method: 'DELETE',
+   }
+   const response = await fetch (url, options)
+
+   return response.ok
+}
